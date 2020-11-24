11066b682c93cf13adf0f10499d47a3e0547e5d8
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2020-11-24 17:19:40 +0530

    -playing sound -linking multiple buttons to IBAction

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("tapped")
+        print(sender.backgroundColor!)
+        
+        /*
+        guard let button = sender as? UIButton else {
+               return
+           }
+        
+       
+        
+        switch button.tag {
+            case 1:
+                playSound("C")
+            case 2:
+                playSound("D")
+            case 3:
+                playSound("E")
+        case 4:
+            playSound("F")
+        case 5:
+            playSound("G")
+        case 6:
+            playSound("A")
+        case 7:
+            playSound("B")
+            default:
+                print("Unknown language")
+                return
+            }
+        
+        */
+        playSound(res: sender.currentTitle!)
+    }
     
-
+    func playSound(res: String) {
+        let url = Bundle.main.url(forResource: res, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
